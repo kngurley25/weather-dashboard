d5199a5bf91bea2fd4427aac83f296e5a3dd13a9
kngurley <kngurley25@gmail.com>
2021-10-22 11:08:12 -0500

    start js functions for form handler and display data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,69 @@
+// from HTML elements
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#city");
+
+var currentWeatherEl = document.querySelector("#current-weather-container");
+var citySearchTerm = document.querySelector("#city-search-term");
 
 
-var lat = 33.44
-var lon = -94.04
+// var lat = 33.44
+// var lon = -94.04
 
-// open weather API call
-var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=914d3b53de5d88e879e5979ff877074b";
+// // open weather API call
+// var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=914d3b53de5d88e879e5979ff877074b";
 
-var response = fetch(apiUrl);
-console.log(response);
+// var response = fetch(apiUrl);
+// console.log(response);
+
+
+var getCityWeather = function (city) {
+
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=914d3b53de5d88e879e5979ff877074b";
+
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data, city);
+                    displayWeather(data, city);
+                });
+            } else {
+                alert("Error: City not found");
+            }
+        })
+}
+
+// print weather data to page
+var displayWeather = function() {
+
+    citySearchTerm.textContent = searchTerm
+
+    var temp = document.createElement("p");
+
+    var wind = document.createElement("p");
+
+    var humidity = document.createElement("p");
+
+    var uvIndex = document.createElement("p");
+
+    
+}
+
+// get value from form input
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    var cityName = cityInputEl.value.trim();
+
+    if (cityName) {
+        getCityWeather(cityName);
+        cityInputEl.value = "";
+    }
+    else {
+        alert("Please enter a city");
+    }
+}
+
+
+// add event listener to form element
+userFormEl.addEventListener("submit", formSubmitHandler);
